1c09df4ec36a10992e40077a8f51d5ca882e5235
Daniel <evansdb0@sewanee.edu>
2017-11-29 01:45:20 -0600

    Working on Global Terrorism notebook (EDA and classification/prediction)

---
+++ shared/preprocessing.py
@@ -0,0 +1,12 @@
+import seaborn as sns
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import sys 
+import os
+
+def percent_missing(X):
+	total = X.isnull().sum().sort_values(ascending=False)
+	percent = (X.isnull().sum()/X.isnull().count()).sort_values(ascending=False)
+	missing_data = pd.concat([total, percent], axis=1, keys=['Total', 'Percent'])
+	return missing_data
